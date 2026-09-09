b83eae765b678c27a4c12896fd5164f9be2ff415
yussufosama <youssef2024dev@gmail.com>
2026-09-09 09:00:52 -0700

    Create bonus.js

---
+++ bonus.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let x=0
+    for(let i=0;i<nums.length;i++){
+        if nums[i] != val
+        nums[x]= nums[i]
+        x++
+    }
+}
